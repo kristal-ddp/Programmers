755a8b75748e119a4cbd24c4c6966843a30fbec7
kristal-ddp <kristal.ddp@gmail.com>
2022-11-22 22:37:38 +0900

    📰 Solve Problem

---
+++ Level1/20. 없는 숫자 더하기.js
@@ -0,0 +1,25 @@
+/*
+문제 설명
+0부터 9까지의 숫자 중 일부가 들어있는 정수 배열 numbers가 매개변수로 주어집니다. numbers에서 찾을 수 없는 0부터 9까지의 숫자를 모두 찾아 더한 수를 return 하도록 solution 함수를 완성해주세요.
+
+제한사항
+1 ≤ numbers의 길이 ≤ 9
+0 ≤ numbers의 모든 원소 ≤ 9
+numbers의 모든 원소는 서로 다릅니다.
+*/
+
+function solution(numbers) {
+    
+    // Solve1
+    // let acc = 0;
+    
+    // for(let i = 0; i < 10; i++) {
+    //     if(!numbers.includes(i)) acc += i;
+    // }
+    
+    // return acc;
+
+    // Solve2
+    return 45 - numbers.reduce((acc, data) => acc + data, 0);
+    
+}
