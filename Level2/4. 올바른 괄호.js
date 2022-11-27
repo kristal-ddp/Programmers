0981fe24d05aed7f01f6255bfcc97a10d05e4341
kristal-ddp <kristal.ddp@gmail.com>
2022-11-27 22:47:32 +0900

    📰 Solve Problem

---
+++ Level2/4. 올바른 괄호.js
@@ -0,0 +1,43 @@
+/*
+문제 설명
+괄호가 바르게 짝지어졌다는 것은 '(' 문자로 열렸으면 반드시 짝지어서 ')' 문자로 닫혀야 한다는 뜻입니다. 예를 들어
+
+"()()" 또는 "(())()" 는 올바른 괄호입니다.
+")()(" 또는 "(()(" 는 올바르지 않은 괄호입니다.
+'(' 또는 ')' 로만 이루어진 문자열 s가 주어졌을 때, 문자열 s가 올바른 괄호이면 true를 return 하고, 올바르지 않은 괄호이면 false를 return 하는 solution 함수를 완성해 주세요.
+
+제한사항
+문자열 s의 길이 : 100,000 이하의 자연수
+문자열 s는 '(' 또는 ')' 로만 이루어져 있습니다.
+*/
+
+function solution(s){
+    
+    let count = 0;
+    
+    // Solve1
+    // 시작은 (, 끝은 )
+    // if(s[0] === ')') return false;
+    // if(s[s.length-1] === '(') return false;
+    
+    // for(let i=0; i<s.length; i++) {
+        
+    //     s[i] === '(' ? count++ : count--;
+        
+    //     // (보다 )가 먼저 나오거나 더 많아지면 false 
+    //     if(count < 0) return false;
+    // }
+
+    // Solve2
+    for(let check of s) {
+        
+        check === '(' ? count++ : count--;
+        
+        // (보다 )가 먼저 나오거나 더 많아지면 false 
+        if(count < 0) return false;
+    }
+    
+    // (, ) 개수가 동일
+    return count === 0 ? true : false;
+    
+}
